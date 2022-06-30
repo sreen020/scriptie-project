10b96572782a282877db481d7f5864a28b79f0cd
SR <sjoerd@MacBook-Pro-van-SR.local>
2022-06-30 16:42:06 +0200

    added start login page

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Button from '../components/Button';
 
 const Expert = () => {
 	const [error, setError] = useState('');
@@ -34,16 +35,68 @@ const Expert = () => {
 	};
 
 	return (
-		<div className="bg-primary-yellow">
-			<h1>Login</h1>
+		<section className="grid grid-cols-2 min-h-screen">
+			<article className="flex flex-col items-center justify-center">
+				<div className="max-w-sm">
+					<h1 className="text-center text-4xl font-black pb-4">Welkom terug</h1>
+					<p className="text-center text-text-light">
+						Log hieronder in om terug te gaan naar jouw favoriete bewegings
+						oefeningen
+					</p>
 
-			<form onSubmit={handleLogin}>
-				<input className="border" type="text" name="username" />
-				<input className="border" type="text" name="password" />
-				<button type="submit">SUBMIT</button>
-			</form>
-			<p>{error}</p>
-		</div>
+					<form onSubmit={handleLogin} className="grid gap-6 pt-10">
+						<div>
+							<label className="font-medium" htmlFor="username">
+								Username
+							</label>
+							<input
+								className="mt-2 text-formtext shadow-form bg-white border-0 rounded-md py-3 px-4 w-full text-xs"
+								type="text"
+								name="username"
+								id="username"
+							/>
+						</div>
+						<div>
+							<label className="font-medium" htmlFor="password">
+								Wachtwoord
+							</label>
+							<input
+								className="mt-2 text-formtext shadow-form bg-white border-0 rounded-md py-3 px-4 w-full text-xs"
+								type="password"
+								name="password"
+								id="password"
+							/>
+						</div>
+						{/* <button type="submit">Log in</button> */}
+						<div className="flex justify-end">
+							<Button type="primary" text="Log in" icon={true} />
+						</div>
+					</form>
+					<p>{error}</p>
+				</div>
+			</article>
+
+			<article
+				className="pt-20 rounded-l-3xl flex flex-col justify-center text-white items-center gap-16"
+				style={{
+					background:
+						'linear-gradient(270deg, rgba(66, 51, 127, 0.9) 8.85%, #130160 100%)',
+				}}
+			>
+				<div className="max-w-md">
+					<h1 className="text-6xl">SO-NUTS</h1>
+					<p>
+						Verbeter jouw fysieke activiteit door bewegingsoefeningen op maat!
+					</p>
+				</div>
+
+				<img
+					className="ml-auto w-9/12"
+					src="/img/screenshot.png"
+					alt="Screenshot of application"
+				/>
+			</article>
+		</section>
 	);
 };
 
