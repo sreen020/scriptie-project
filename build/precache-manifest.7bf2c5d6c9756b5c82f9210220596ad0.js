e44981829631138ea1d6b5e88bedb432a5c23dc1
SR <sjoerd@mbp-van-sr.kpn>
2022-09-15 17:34:43 +0200

    build project for deployment

---
+++ build/precache-manifest.7bf2c5d6c9756b5c82f9210220596ad0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e7f3e36b54b0f1f63eeb7b249b5d302",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e530d40d4aec99017a21",
+    "url": "/static/js/2.4e86ecd8.chunk.js"
+  },
+  {
+    "revision": "0c68f995d17db03796b0c760a1972421",
+    "url": "/static/js/2.4e86ecd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19035735d5d1f37a53a6",
+    "url": "/static/js/main.800cbff8.chunk.js"
+  },
+  {
+    "revision": "79592f005b5aa706a3cc",
+    "url": "/static/js/runtime-main.a3c4c95d.js"
+  }
+]);
